13c513cd403845df40675e38e4b060f4122ca4b9
Nick <mr.upgrade3@protonmail.com>
2022-06-25 02:54:09 +0300

    Update

---
+++ script/script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+let counter = 0;
+
+function add_count() {
+    let integer = (() => {
+        return counter++;
+    });
+    let tag = document.createElement("p");
+    let text;
+    text = document.createTextNode(integer());
+    tag.appendChild(text);
+    let element = document.getElementsByTagName("body")[0];
+    element.appendChild(tag)
+};
+
+
